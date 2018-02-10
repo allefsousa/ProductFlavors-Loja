a70eee9aac6f0029e4269d9cc7689ed63df2e0ee
Allef Sousa <allefsousa_1@hotmail.com>
2018-02-10 21:24:55 -0200

    mudando o icone e o nome do app apartir de um dos Flavors nesse caso a LOja masculina

--- app/src/masculina/java/br/com/allefdeveloper/lojaonline/masculina/CatalogoMasculinoActivity.java
+++ app/src/masculina/java/br/com/allefdeveloper/lojaonline/masculina/CatalogoMasculinoActivity.java
@@ -3,11 +3,14 @@ package br.com.allefdeveloper.lojaonline.masculina;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import br.com.allefdeveloper.lojaonline.R;
+
 public class CatalogoMasculinoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalogo_masculino);
+        getSupportActionBar().setTitle(R.string.too);
     }
 }

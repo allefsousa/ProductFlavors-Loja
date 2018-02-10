6349de019351c728ebeb9a8075cbcb655eb4f583
Allef Sousa <allefsousa_1@hotmail.com>
2018-02-10 20:17:49 -0200

    criacao dos pacotes com os nomes dos flavors na pasta App/src

---
+++ app/src/feminina/java/br/com/allefdeveloper/lojaonline/feminina/CatologoFemininoActivity.java
@@ -0,0 +1,15 @@
+package br.com.allefdeveloper.lojaonline.feminina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.allefdeveloper.lojaonline.R;
+
+public class CatologoFemininoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catologo_feminino);
+    }
+}

48f5c42c4f0d710f0975a3ef92c64fe3136a2779
Allef Sousa <allefsousa_1@hotmail.com>
2018-02-11 00:28:33 -0200

    biblioteca Shimmer do facebook adicionada para dar efeito ao carreganmento de imagem

--- app/src/main/java/br/com/allefdeveloper/lojaonline/PrincipalActivity.java
+++ app/src/main/java/br/com/allefdeveloper/lojaonline/PrincipalActivity.java
@@ -3,11 +3,17 @@ package br.com.allefdeveloper.lojaonline;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.shimmer.ShimmerFrameLayout;
+
 public class PrincipalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        ShimmerFrameLayout container =
+                (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container);
+        container.startShimmerAnimation();
     }
 }
